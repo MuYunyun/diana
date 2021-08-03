37267d3f1b7d6d476622cec35ad3fe2ce45bee48
MuYunyun <328375795@qq.com>
2021-08-03 18:32:38 +0800

    feat: update

--- src/curry.ts
+++ src/curry.ts
@@ -1,3 +1,4 @@
+
 // 函数柯里化定义：将接受多个入参的函数转化为接受一个入参的函数
 /**
  * 函数柯里化。如果提供的参数 (args) 数量足够，则调用传递函数 f ，否则返回一个 curried 函数 f 。
@@ -6,8 +7,7 @@
  * @param {*} args
  * _.curry(Math.pow)(2)(10) => 1024
  */
-// todo
-function curry(fn: any, arity: number = fn.length, ...args: any[]) {
+function curry(fn: any, arity: number = fn.length, ...args: any[]): any {
   return arity <= args.length
     ? fn(...args)
     : curry.bind(null, fn, arity, ...args)
